209f648676ad2ff68c498ad28b4f60252d6e8616
Shweh <cshweh00@gmail.com>
2025-09-10 14:27:31 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 构建时导出纯静态网站到 out/
   output: 'export',
-  // 如果以后用 next/image，这句能避免导出时报错
   images: { unoptimized: true }
 };
 module.exports = nextConfig;
